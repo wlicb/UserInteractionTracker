45a1cb501ee71fa484169b928862899e1a062212
Ziyi <wangziyi000128@gmail.com>
2025-03-05 20:36:51 +0000

    frontend test

---
+++ frontend/script.js
@@ -0,0 +1,4 @@
+// This file can be used for adding interactive elements to the website.
+
+// Example: Add a simple console log to verify the script is loaded
+console.log('Script loaded successfully.'); 
